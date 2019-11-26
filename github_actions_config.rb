23cc5649507f8c2ad695f2a016e77f0059b3154d
KOBAYASHI Shuji <shuujii@gmail.com>
2019-11-26 21:06:26 +0900

    update windows.yml [skip appveyor]

--- github_actions_config.rb
+++ github_actions_config.rb
@@ -2,6 +2,7 @@ MRuby::Lockfile.disable
 MRuby::Build.new do |conf|
   conf.toolchain :visualcpp
   conf.enable_debug
+  conf.enable_test
   conf.gem core: 'mruby-bin-mruby'
   conf.gem core: 'mruby-print'
 end
